0849dc5340e1789b0b49c65f4fdea8a7f06c87f1
Yesvanthraja D <yesvanthraja.ad20@bitsathy.ac.in>
2022-05-12 21:24:02 -0700

    Update remedies.py

--- disease_prediction/remedies.py
+++ disease_prediction/remedies.py
@@ -1,3 +1,3 @@
 remedies = {
-    "gerd": "Chew gum \n Sleep on your left side \n Elevate the head of your bed. /n Eat dinner earlier. /n Opt for cooked onions instead of raw. /n Eat smaller, more frequent meals. /n Maintain a moderate weight. ... /n Follow a low carb diet."
+    "gerd": "Chew gum \n Sleep on your left side \n Elevate the head of your bed. \n Eat dinner earlier. \n Opt for cooked onions instead of raw. \n Eat smaller, more frequent meals. \n Maintain a moderate weight. \n Follow a low carb diet."
 }
