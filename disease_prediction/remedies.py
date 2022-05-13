288dff5ff2b8305c8350a5112dd5f28292d09929
Yesvanthraja D <yesvanthraja.ad20@bitsathy.ac.in>
2022-05-12 22:38:38 -0700

    Update remedies.py

--- disease_prediction/remedies.py
+++ disease_prediction/remedies.py
@@ -1,4 +1,11 @@
-remedies = {
-    "gerd": "<ol> <li>Sleep on your left side</li> <li>Chew gum</li><li>Elevate the head of your bed</li></ol>"
- 
-}
+remedies = {"gerd": "Sleep on your left side.\nChew gum.\nElevate the head of your bed.",
+    "fungal infection":"Wash with Soap and Water.\nUse Apple Cider Vinegar.\nUse Tea Tree Oil.\nUse Coconut Oil. \nUse Turmeric.",
+    "allergy":"Oatmeal bath.\nAloe vera",
+    "diabetes":"Leafy Greens.\nFatty Fish.\nNuts and Eggs.\nSeeds.\nNatural Fats.\nApple Cider Vinegar.",
+    "gastroenteritis":"Ease back into eating.\nAvoid certain foods and substances until you feel better.\nGet plenty of rest.\nTry anti-diarrhea medications.",
+    "bronchial asthma":"Buteyko Breathing Technique.\nPapworth method.\nGarlic.\nGinger.\nHoney.",
+    "malaria":"Grapefruit.\nHoly Basil\nAvoiding Mosquitoes.\nApple Cider Vinegar.\nFever Nut.",
+    "chicken pox":"Apply calamine lotion.\nServe sugar-free popsicles.\nBathe in oatmeal.\nWear mittens to prevent scratching.",
+    "common cold":"Rest. Your body needs rest to heal.\nSoothe a sore throat.\nCombat stuffiness.\nRelieve pain.\nSip warm liquids.",
+    "pneumonia":" Rest.\nAntibiotics.\nFluid intake." }
+    
